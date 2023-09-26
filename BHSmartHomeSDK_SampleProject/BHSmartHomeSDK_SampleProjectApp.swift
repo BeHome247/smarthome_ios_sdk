5adfadd7ea472f2fca988d9e0d862c3d59a73f1b
Maximiliano Chiesa <mchiesa@buildingengines.com>
2023-09-26 12:13:37 -0300

    Initial Commit

---
+++ BHSmartHomeSDK_SampleProject/BHSmartHomeSDK_SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BHSmartHomeSDK_SampleProjectApp.swift
+//  BHSmartHomeSDK_SampleProject
+//
+//  Created by Maximiliano Chiesa on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BHSmartHomeSDK_SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BHSmartHomeSDK_SampleProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BHSmartHomeSDK_SampleProject
+//
+//  Created by Maximiliano Chiesa on 9/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
